596b4d49a0a0a8b82672a9eafa63ea617316ed7f
Tia Leo <info@devopseasylearning>
2024-04-23 21:34:46 -0500

    DevOps Easy Learning

--- terraform/modules/s3-backend/main.tf
+++ terraform/modules/s3-backend/main.tf
@@ -14,3 +14,15 @@ resource "aws_s3_bucket_versioning" "versioning_example" {
     status = "Enabled"
   }
 }
+
+resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
+  name           = "s6-terraform-s3-lock"
+  hash_key       = "LockID"
+  read_capacity  = 20
+  write_capacity = 20
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
